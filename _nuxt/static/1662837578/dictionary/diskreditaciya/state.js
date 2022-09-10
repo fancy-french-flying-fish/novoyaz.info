aef318b2f50c2b37e85f225135b6164b633affdc
olegpopovskiy <olegpopovskiy@users.noreply.github.com>
2022-09-10 20:53:25 +0000

    deploy: 544c8ed49f535d2c98ac3c61980f326be134236c

--- _nuxt/static/1662837578/dictionary/diskreditaciya/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnovoyaz.info\u002F_nuxt\u002Fstatic\u002F1662837578",layout:"default",error:a,serverRendered:true,routePath:"\u002Fdictionary\u002Fdiskreditaciya",config:{_app:{basePath:"\u002Fnovoyaz.info\u002F",assetsPath:"\u002Fnovoyaz.info\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"9057175c"}}}}(null));
---
+++ _nuxt/static/1662843203/dictionary/jestkaya-posadka/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1662843203",layout:"default",error:a,serverRendered:true,routePath:"\u002Fdictionary\u002Fjestkaya-posadka",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"d135ec3e"}}}}(null));
