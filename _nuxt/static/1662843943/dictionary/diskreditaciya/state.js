b0f8bab8e2175a6ef78c1c762f5a2a69972546cf
olegpopovskiy <olegpopovskiy@users.noreply.github.com>
2022-09-10 21:54:10 +0000

    deploy: 2683e9982964e6a49b352ac4a02e5a34b338ccf2

--- _nuxt/static/1662843943/dictionary/diskreditaciya/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1662843943",layout:"default",error:a,serverRendered:true,routePath:"\u002Fdictionary\u002Fdiskreditaciya",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"c70686fc"}}}}(null));
