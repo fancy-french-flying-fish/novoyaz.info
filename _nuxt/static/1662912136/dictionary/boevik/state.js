d34129ba1a0b5c3ec1ca65dc10921d6c83a65074
olegpopovskiy <olegpopovskiy@users.noreply.github.com>
2022-09-11 16:02:18 +0000

    deploy: e42755e04d01bf871ba594f02874b03cfc36861f

---
+++ _nuxt/static/1662912136/dictionary/boevik/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1662912136",layout:"default",error:a,serverRendered:true,routePath:"\u002Fdictionary\u002Fboevik",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"191b135d"}}}}(null));
