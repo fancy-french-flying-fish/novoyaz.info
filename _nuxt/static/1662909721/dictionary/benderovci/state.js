ad2a8502ea7309c64f732ba073d549f2d59c1ead
olegpopovskiy <olegpopovskiy@users.noreply.github.com>
2022-09-11 15:22:03 +0000

    deploy: 53489668b6a28c1820e90aec142bdc4503d36ceb

---
+++ _nuxt/static/1662909721/dictionary/benderovci/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1662909721",layout:"default",error:a,serverRendered:true,routePath:"\u002Fdictionary\u002Fbenderovci",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"1e1f3c8e"}}}}(null));
