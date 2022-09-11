d891c3b263a791438b4db4a34b88246390a5dee2
olegpopovskiy <olegpopovskiy@users.noreply.github.com>
2022-09-11 16:09:13 +0000

    deploy: a0e1beacc3d2fa4af0f9ec4283815c5944407223

---
+++ _nuxt/static/1662912551/dictionary/hlopok/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1662912551",layout:"default",error:a,serverRendered:true,routePath:"\u002Fdictionary\u002Fhlopok",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"5c6b7ac5"}}}}(null));
